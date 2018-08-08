600c9e642889b33285217ff65e14421c9850989d
Adriana <adpc1609@gmail.com>
2018-08-07 23:42:29 -0400

    mostrar saldo listo

--- src/js/bip.js
+++ src/js/bip.js
@@ -1,7 +1,22 @@
+const inputText = document.querySelector('input');
 
-fetch('http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=23501780')
-.then(response => response.json())
-.then(data => {
-  console.log(data) // Prints result from `response.json()` in getRequest
+inputText.addEventListener('keypress', (event) => {
+  let key = event.which || event.keyCode;
+  if (key==13) {
+      let idBip = inputText.value;
+      console.log(idBip);
+      inputText.value = '';
+      fetch(`http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${idBip}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+          let render = '';
+         // console.log('mi objeto es' + myObject);
+         render += `<div`;
+         render += `<p>Saldo : ${data.saldoTarjeta}</p>`;
+         render += `</div>`;
+          appContainer.innerHTML = render;
+      })
+      .catch(error => console.error(error))
+  }
 })
-.catch(error => console.error(error))
