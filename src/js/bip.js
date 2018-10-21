b9231c14233352ee533d7654e3c36ec0335a9b1e
Adriana Perez <adpc1609@gmail.com>
2018-10-21 02:31:10 -0300

    mejorando diseño

--- src/js/bip.js
+++ src/js/bip.js
@@ -1,58 +1,85 @@
-window.getBip = ()=>{
+window.getBip = () => {
   let idBip = document.getElementById("numBip").value;
   console.log(idBip);
   let url = `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${idBip}`;
   fetch(url)
-  .then(response => response.json())
-  .then(idBip => {
-    console.log(idBip);
-    infoSaldo(idBip);
-  })
-  .catch(error => {
-     console.log(error); 
-  });
+    .then(response => response.json())
+    .then(idBip => {
+      console.log(idBip);
+      infoSaldo(idBip);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
-let divSaldo = document.getElementById('divSaldo');
-let infoSaldo = (idBip) =>{
-let render = '';
-  render += `<div class="container my-3">`;
-  render += `<div class="row">`;
-  render += `<div class="col-sm-12 text-center">`;
-  render += `<div class="mostrarSaldo">`
-  render += `<p class="titulo">SALDO TOTAL</p>`;
-  render += `</div>`
-  render += `<div class="mostrarSaldo2">`
-  render += `<p class="saldo"> ${idBip.saldoTarjeta}</p>`;
-  render += `</div>`;
-  render += `</div>`;
-  render += `</div>`;
-  divSaldo.innerHTML = render;
+  let divSaldo = document.getElementById('divSaldo');
+  let infoSaldo = (idBip) => {
+    let render = '';
+    render += `<div class="container my-3">`;
+    render += `<div class="row">`;
+    render += `<div class="col-sm-12 text-center">`;
+    render += `<div class="mostrarSaldo">`
+    render += `<p class="titulo">SALDO TOTAL</p>`;
+    render += `</div>`
+    render += `<div class="mostrarSaldo2">`
+    render += `<p class="saldo"> ${idBip.saldoTarjeta}</p>`;
+    render += `</div>`;
+    render += `</div>`;
+    render += `</div>`;
+    divSaldo.innerHTML = render;
+  };
+}
+
+window.calculatorTarifa = () => {
+  let idBip = document.getElementById('numBip').value;
+  let url = `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${idBip}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(idBip => {
+      calculator(idBip);
+      console.log
+    })
+    .cath(error => {
+      console.log(error);
+
+    });
+   
+  let calculator = (idBip) => {
+    let selector = document.getElementById("selector");
+    let opciones = document.getElementsByTagName("option")[selector].value;
+    infoSaldoActual = idBip[saldoTarjeta].replace(/[$,.]+/g,"");
+    let costoPasaje = document.getElementById("pasaje");
+    let saldoRestante = document.getElementById("saldoRestante");
+    if (opciones == 1) {
+      TarifaAlta = 760;
+      saldoFinal = infoSaldoActual - TarifaAlta;
+      let costoPasaje = '';
+      render += `<div class="container my-3">`;
+      render += `<div class="row">`;
+      render += `<div class="col-sm-12 text-center">`;
+      render += `<div class="mostrarSaldo">`
+      render += `<p class="titulo">COSTO PASAJE</p>`;
+      render += `</div>`
+      render += `<div class="mostrarSaldo2">`
+      render += `<p class="saldo">$+costoPasaje+</p>`;
+      render += `</div>`;
+      render += `</div>`;
+      render += `</div>`;
+      pasaje.innerHTML = costoPasaje;
+
+      let saldoRestante = '';
+      render += `<div class="container my-3">`;
+      render += `<div class="row">`;
+      render += `<div class="col-sm-12 text-center">`;
+      render += `<div class="mostrarSaldo">`
+      render += `<p class="titulo">SALDO FINAL</p>`;
+      render += `</div>`
+      render += `<div class="mostrarSaldo2">`
+      render += `<p class="saldo">$+saldoFinal+</p>`;
+      render += `</div>`;
+      render += `</div>`;
+      render += `</div>`;
+      saldoRestante.innerHTML = saldoRestante;
+    }
+  }
 };
-}
-
-window.calculatorTarifa = ()=>{ 
-let idBip = document.getElementById('numBip').value;
-let url = `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${idBip}`;
-fetch(url)
-.then(response => response.json())
-.then(idBip =>{
-  calculator(idBip);
-})
-.cath(error =>{
-  console.log(error);
-
-});
-}
-/* calculator = (idBip) =>{
-  let select = document.getElementById('inputGroupSelect01');
-  var value = select.options[select.selectedIndex].value;
-  console.log(value);
-
-  saldoDeTarjeta = idBip["saldoTarjeta"].replace
- 
- if (select == 1){
-   let TarifaAl = 760;
-  
- }
-}
- */
